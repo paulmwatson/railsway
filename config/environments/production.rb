33c6e848e3bc644563a312b97a9b431bef9fa193
paulmwatson <github.com@paulmwatson.com>
2023-09-29 17:50:08 +0200

    Temporary logging to debug config.hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << ENV['RAILWAY_PUBLIC_DOMAIN']
-
+  logger.debug "config.hosts: #{config.hosts}"
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
